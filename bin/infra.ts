0fdbcf9de8e99fbf88409be4d7d612fab345495a
Maksym Martynenko <maksym_martynenko@epam.com>
2025-10-20 18:25:21 +0000

    feat: attach lambda authorizer to the products importer

--- bin/infra.ts
+++ bin/infra.ts
@@ -2,10 +2,14 @@
 import * as cdk from "aws-cdk-lib";
 import { ProductsServiceStack } from "../lib/products-service-stack";
 import { ImportServiceStack } from "../lib/import-service-stack";
+import { AuthorizationServiceStack } from "../lib/authorization-service-stack";
 
 const app = new cdk.App();
 
+const authorizationServiceStack = new AuthorizationServiceStack(app, "AuthorizationServiceStack");
 const productsServiceStack = new ProductsServiceStack(app, "ProductsServiceStack");
+
 new ImportServiceStack(app, "ImportServiceStack", {
-	catalogItemsQueue: productsServiceStack.productsService.catalogItemsQueue,
+  catalogItemsQueue: productsServiceStack.productsService.catalogItemsQueue,
+  authorizerFunction: authorizationServiceStack.authorizationService.basicAuthorizerFunction,
 });
